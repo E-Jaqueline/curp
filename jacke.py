2994c6f0b2bedefe32ba8fbf1a7668569dea2979
unknown <jaquelinepicazoosorio@gmail.com>
2024-03-01 17:51:25 -0600

    segundo cambio

--- jacke.py
+++ jacke.py
@@ -1,1 +1,12 @@
 
+nombre = input("cual es tu nombre")
+print (nombre)
+apellido = input("cual es tu apellido")
+print(apellido)
+edad =input("cual es tu edad")
+print(edad)
+m = nombre
+print(m[0])
+d = apellido
+print(d[0])
+print(m[0] + edad + d[0])
